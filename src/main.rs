254c57e5b7892394a2f709322af15de2680c7ecf
Cyanism <61608977+Cyanistic@users.noreply.github.com>
2023-08-05 22:34:16 -0400

    added terminal check

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{collections::HashMap, process::exit};
-use std::io::{stdout, Write};
+use std::io::{stdout, Write, IsTerminal};
 
 fn main() {
     let mut sequences: HashMap<char, String> = HashMap::from_iter([
@@ -104,20 +104,37 @@ fn print_help() -> Result<(), std::io::Error>{
     const BOLD: &str = "\x1b[1m";
     const UND: &str = "\x1b[4m";
     const RES: &str = "\x1b[0m";
-    writeln!(stdout(), "{}Escapes (or unescapes) special character sequences and prints the result to stdout.\n", BOLD)?;
-    writeln!(stdout(), "{}{}Usage:{}{} escaper [OPTIONS] [SEQUENCES...]{}\n", BOLD, UND, RES, BOLD, RES)?;
-    writeln!(stdout(), "{}{}OPTIONS:{}", BOLD, UND, RES)?;
-    writeln!(stdout(), "  {}-h, --help                    {}Print the help information and exit.", BOLD, RES)?;
-    writeln!(stdout(), "  {}-V, --version                 {}Print version and exit.", BOLD, RES)?;
-    writeln!(stdout(), "  {} -                            {}Reads SEQUENCES from stdin.", BOLD, RES)?;
-    writeln!(stdout(), "  {}--                            {}Causes all further arguments to be read as SEQUENCES.", BOLD, RES)?;
-    writeln!(stdout(), "  {}-s, --string                  {}Uses string ($) escape sequences instead of default URL (%) escape sequences.", BOLD, RES)?;
-    writeln!(stdout(), "  {}-r, --regex                   {}Uses shell/regex (\\) escape sequences instead of default URL (%) escape sequences.", BOLD, RES)?;
-    writeln!(stdout(), "  {}-u, --undo                    {}Unescapes SEQUENCES.", BOLD, RES)?;
-    writeln!(stdout(), "{}{}EXCLUSION OPTIONS:{}", BOLD, UND, RES)?;
-    writeln!(stdout(), "  {}-a, --apostrophes             {}Removes apostrophes from escapable character list.", BOLD, RES)?;
-    writeln!(stdout(), "  {}-b, --backticks               {}Removes backticks from escapable character list.", BOLD, RES)?;
-    writeln!(stdout(), "  {}-q, --quotes                  {}Removes quotation marks from escapable character list.", BOLD, RES)?;
+    if stdout().is_terminal() {
+        writeln!(stdout(), "{}Escapes (or unescapes) special character sequences and prints the result to stdout.\n", BOLD)?;
+        writeln!(stdout(), "{}{}Usage:{}{} escaper [OPTIONS] [SEQUENCES...]{}\n", BOLD, UND, RES, BOLD, RES)?;
+        writeln!(stdout(), "{}{}OPTIONS:{}", BOLD, UND, RES)?;
+        writeln!(stdout(), "  {}-h, --help                    {}Print the help information and exit.", BOLD, RES)?;
+        writeln!(stdout(), "  {}-V, --version                 {}Print version and exit.", BOLD, RES)?;
+        writeln!(stdout(), "  {} -                            {}Reads SEQUENCES from stdin.", BOLD, RES)?;
+        writeln!(stdout(), "  {}--                            {}Causes all further arguments to be read as SEQUENCES.", BOLD, RES)?;
+        writeln!(stdout(), "  {}-s, --string                  {}Uses string ($) escape sequences instead of default URL (%) escape sequences.", BOLD, RES)?;
+        writeln!(stdout(), "  {}-r, --regex                   {}Uses shell/regex (\\) escape sequences instead of default URL (%) escape sequences.", BOLD, RES)?;
+        writeln!(stdout(), "  {}-u, --undo                    {}Unescapes SEQUENCES.", BOLD, RES)?;
+        writeln!(stdout(), "{}{}EXCLUSION OPTIONS:{}", BOLD, UND, RES)?;
+        writeln!(stdout(), "  {}-a, --apostrophes             {}Removes apostrophes from escapable character list.", BOLD, RES)?;
+        writeln!(stdout(), "  {}-b, --backticks               {}Removes backticks from escapable character list.", BOLD, RES)?;
+        writeln!(stdout(), "  {}-q, --quotes                  {}Removes quotation marks from escapable character list.", BOLD, RES)?;
+    }else{
+        writeln!(stdout(), "Escapes (or unescapes) special character sequences and prints the result to stdout.\n")?;
+        writeln!(stdout(), "Usage: escaper [OPTIONS] [SEQUENCES...]\n")?;
+        writeln!(stdout(), "OPTIONS:")?;
+        writeln!(stdout(), "  -h, --help                    Print the help information and exit.")?;
+        writeln!(stdout(), "  -V, --version                 Print version and exit.")?;
+        writeln!(stdout(), "   -                            Reads SEQUENCES from stdin.")?;
+        writeln!(stdout(), "  --                            Causes all further arguments to be read as SEQUENCES.")?;
+        writeln!(stdout(), "  -s, --string                  Uses string ($) escape sequences instead of default URL (%) escape sequences.")?;
+        writeln!(stdout(), "  -r, --regex                   Uses shell/regex (\\) escape sequences instead of default URL (%) escape sequences.")?;
+        writeln!(stdout(), "  -u, --undo                    Unescapes SEQUENCES.")?;
+        writeln!(stdout(), "EXCLUSION OPTIONS:")?;
+        writeln!(stdout(), "  -a, --apostrophes             Removes apostrophes from escapable character list.")?;
+        writeln!(stdout(), "  -b, --backticks               Removes backticks from escapable character list.")?;
+        writeln!(stdout(), "  -q, --quotes                  Removes quotation marks from escapable character list.")?;
+    }
     exit(0);
 }
 
