5d9d6cdb70b2fc0e377daa42cf1f0a1107abddd4
Cyanism <61608977+Cyanistic@users.noreply.github.com>
2023-08-13 01:26:42 -0400

    removed useless build

--- build.rs
+++
@@ -1,9 +0,0 @@
-use std::{process::Command, fs::File, io::Write};
-
-
-fn main() -> Result<(), std::io::Error>{
-    let man_data = Command::new("help2man").arg("escaper").output()?.stdout;
-    let mut file = File::create("escaper-gen.1")?;
-    file.write_all(&man_data)?;
-    Ok(())
-}
